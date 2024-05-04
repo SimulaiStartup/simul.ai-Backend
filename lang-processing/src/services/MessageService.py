a70042256bf04e76c46a3a9bb1bffbd57354ff0a
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2024-05-04 20:06:32 -0300

    updating csv

--- lang-processing/src/services/MessageService.py
+++ lang-processing/src/services/MessageService.py
@@ -29,12 +29,11 @@ def fetchData(message:str, id:int) -> str:
 
     df_script = df_script[df_script['ROTEIRO_ID'] == id_script]
 
-    prompt = buildPrompt(context=context, df_scripts=df_script, message=message)
-
+    prompt = buildPrompt(context=context, df_scripts=df_script.iloc[0], message=message)
 
     stage = context['STAGE'].max()+1
+    m_id = context['MENSAGEM_ID'].max()
 
-    print(stage)
 
     df_script_stages = pd.read_csv("../../data/roteiro_stages.csv", sep=";", encoding="UTF-8")
 
@@ -43,25 +42,43 @@ def fetchData(message:str, id:int) -> str:
     options = df_script_stages[df_script_stages['STAGE'] == stage]
     options = options['OPTION'].astype(str).to_list()
 
-    print(options)
+    response = getResponse(options=options, prpt=prompt)
+
+    print(response)
+    print(options[response-1])
+
+    data = {
+        "CONVERSATION_ID" : [id, id], 
+        "ROTEIRO_ID" : [id_script, id_script], 
+        "STAGE" : [stage, stage], 
+        "MENSAGEM_ID" : [m_id+1, m_id+2], 
+        "TRANSCRIPT" : [message, options[response-1]], 
+        "SENDER": ["USER", "CHAT"]
+    }
 
 
-    return getResponse(options=options, prpt=prompt)
+    df = pd.DataFrame(data)
+
+    print(df.head())
+    
+    df.to_csv('../../data/transcripts.csv', mode='a', index=False, header=False, sep=";")
+
+    return response
 
 
 
-def buildPrompt(context: pd.DataFrame, df_scripts: pd.DataFrame, message:str):
+def buildPrompt(context: pd.DataFrame, df_scripts: pd.Series, message:str):
 
-    prompt = df_scripts["CONTEXT"].to_string() + "\n\n Essa é a conversa até agora: \n"
+    prompt = df_scripts["CONTEXT"] + "\n\n Essa é a conversa até agora: \n"
 
     messages = context['TRANSCRIPT'].to_list()
     senders = context['SENDER'].to_list()
 
 
     for i in range(len(context)):
-        prompt += df_scripts[senders[i]].to_string() + " - " + messages[i] + "\n\n"
+        prompt += df_scripts[senders[i]] + " - " + messages[i] + "\n\n"
 
-    prompt += "A última mensagem do/a " + df_scripts["USER"].to_string() + " foi:\n" + message
+    prompt += "A última mensagem do/a " + df_scripts["USER"] + " foi:\n" + message
 
     prompt+= '''
 Qual seria a próxima resposta mais apropriada, entre as seguintes:
@@ -78,11 +95,13 @@ Responda somente com um número, correspondente a resposta escolhida
     return prompt
 
 
-def getResponse(options: List[str], prpt: str) -> str:
+def getResponse(options: List[str], prpt: str) -> int:
 
     prompt = ChatPromptTemplate.from_template(prpt)
     output_parser = StrOutputParser()
 
     chain = prompt | MODEL | output_parser
 
-    return chain.invoke({f"Resposta{i+1}":options[i] for i in range(len(options))})
+    n = chain.invoke({f"Resposta{i+1}":options[i] for i in range(len(options))})
+
+    return int(n)
