9af72d7751d202504543e5540f7d7450b1440951
PedroPertusi <pedrovazpertusi@gmail.com>
2024-05-06 14:20:31 -0300

    adding cors policy

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
-# run: uvicorn main:app --reload
-
-from fastapi import FastAPI # type: ignore
-from typing import List
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src import ConversationController
 
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, you can restrict this to specific domains
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],  # Allow only specific HTTP methods
+    allow_headers=["*"],  # Allow all headers, you can restrict this as needed
+)
+
+# Include your router
 app.include_router(ConversationController.router)
 
+# Define root route handler
 @app.get("/")
 def root():
     return {"Hello": "World"}
