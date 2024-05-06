b787d1e14354c98bc528a31246f5d519a935e8dc
EduardoMVAz <emendesvaz@gmail.com>
2024-05-06 14:39:15 -0300

    youtube teste

--- app/src/ConversationController.py
+++ app/src/ConversationController.py
@@ -20,3 +20,7 @@ class RequestBody(BaseModel):
 def process_and_answer(conversa_id: int, roteiro_id: int, body: RequestBody):
     user_response = speech_to_text(body.URL)
     return fetchData(conversa_id, roteiro_id, user_response)
+
+@router.post("youtube/", response_model=str)
+def youtube():
+    return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
